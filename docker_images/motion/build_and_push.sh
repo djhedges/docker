25eb866b96cd0c4078bca38187d2048c736c9c9b
djhedges <djhedges@gmail.com>
2023-02-25 20:54:58 -0800

    Update to the buildx foo

--- docker_images/motion/build_and_push.sh
+++ docker_images/motion/build_and_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set +e
-set +x
-docker build . -t djhedges/docker:motion
+set -e
+set -x
+docker buildx create --use
+docker buildx build . -t djhedges/docker:motion --platform linux/arm/v7,linux/arm64,linux/arm/v8,linux/amd64
 docker push djhedges/docker:motion
